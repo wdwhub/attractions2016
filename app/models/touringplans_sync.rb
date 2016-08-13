70d360924235dc8d9274a2b2ecf50ead2186ae13
Carl Tanner <rocketeer.captproton@gmail.com>
2016-08-13 13:45:00 -0700

    INIT: touringplans connection, client, venue cache

--- app/models/touringplans_sync.rb
+++ app/models/touringplans_sync.rb
@@ -1,23 +1,67 @@
-require_relative "representations/attraction"
+require_relative "./representations/attraction"
 
 class TouringplansSync
   
-  attr_accessor :client
+  attr_accessor :client, :routes, :venue_default
 
-  def initialize(client: TouringplansClient.new)
-    @client = client
+  def initialize(client: TouringplansClient.new, routes: TouringplansClient.new.routes)
+    @client         = client
+    @routes         = routes
+    @venue_default  = TouringplansMissingVenue.new
   end
 
   def title
     "I am touring plans park sync"
   end
   
+  def update_park_venues(park_route_key:)
+    list = list_park_venues(route: park_route_key)
+    # list.collect { |venue| venue.name }
+    list.each do |venue_representation|
+      updated_venue = update_cached_touring_plans_venue_from_touringplans_com(
+        route: park_route_key, 
+        representation:  venue_representation)
+      puts updated_venue
+    end
+  end
+  
   def list_park_venues(route:)
-    client.permalink        = ""
+    client.permalink          = ""
     response                  = client.send(route)
     result                    = Representation.new(
         {"attractions" => response}
       )
+      .attractions
   end
   
+  def collect_touringplans_venue_representation(route:, permalink:)
+    client.permalink          = permalink
+    response                  = client.send(route)
+    result                    = Representation.new(
+        {"attractions" => [response]}
+      )
+  end
+  
+  def update_cached_touring_plans_venue_from_touringplans_com(route:, representation: )
+    return venue_default if representation.permalink.to_s.length == 0
+    
+    remote_touringplans_venue = self.collect_touringplans_venue_representation(
+        route: route, 
+        permalink: representation.permalink
+      )
+      .attractions
+      .first
+      
+    remote_touringplans_venue.permalink = representation.permalink
+    tp_cached_venue  = TouringplansCachedVenue.where(
+      permalink: remote_touringplans_venue.permalink
+      ).first_or_create
+    
+    tp_cached_venue.update(remote_touringplans_venue.to_h)
+    puts "==== #{tp_cached_venue.permalink} ==="
+    puts tp_cached_venue.attributes
+    puts "======================="
+    
+    
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,60 @@
 FactoryGirl.define do
-  factory :touringplans_sync do
-    
-  end
-  factory :touringplans_client do
-    
-  end
-  factory :touringplans_connection do
-    
+  factory :touringplans_cached_venue do
+    name "MyString"
+    fastpass_booth false
+    short_name "MyString"
+    open_emh_morning false
+    open_emh_evening false
+    single_rider false
+    time_zone "MyString"
+    seasonal false
+    open_very_merry false
+    open_not_so_scary false
+    category_code "MyString"
+    duration "9.99"
+    scheduled_code "MyString"
+    what_it_is "MyString"
+    scope_and_scale_code "MyString"
+    when_to_go "MyString"
+    average_wait_per_hundred "MyString"
+    average_wait_assumes "MyString"
+    loading_speed "MyString"
+    probable_wait_time "MyString"
+    special_needs "MyString"
+    height_restriction "MyString"
+    intense false
+    extinct_on "2016-08-13 10:58:13"
+    opened_on "2016-08-13 10:58:13"
+    frightening false
+    physical_considerations false
+    handheld_captioning false
+    video_captioning false
+    reflective_captioning false
+    assistive_listening false
+    audio_description false
+    wheelchair_transfer_code "MyString"
+    no_service_animals false
+    sign_language false
+    service_animal_check false
+    not_to_be_missed false
+    rider_swap false
+    ultimate_code "MyString"
+    ultimate_task "MyString"
+    park_entrance "MyString"
+    relative_open "MyString"
+    relative_close "MyString"
+    close_at_dusk false
+    fastpass_available false
+    crowd_calendar_version 1
+    match_name "MyString"
+    crazy_threshold 1
+    fastpass_only false
+    allow_showtimes_after_close false
+    disconnected_fastpass_booth false
+    crowd_calendar_group 1
+    arrive_before "MyString"
+    arrive_before_fp "MyString"
+    opens_at_sunset false
+    closes_at_sunset false
   end
 end
