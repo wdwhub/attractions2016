d54c08db03cb8cafbcf3137e47bb7c8d8d8c29a8
Carl Tanner <rocketeer.captproton@gmail.com>
2016-08-20 09:38:05 -0700

    ADDED: route to sidekiq to monitor background jobs and connections

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   ActiveAdmin.routes(self)
   resources :attractions, only: [:index, :show]
   root to: 'high_voltage/pages#show', id: 'home'
